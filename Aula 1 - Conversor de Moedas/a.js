8eedb5a68283b5c7c0a332112249c7531f229e54
SamukaCode <samuelpcamargo2005@gmail.com>
2023-01-30 15:44:45 -0300

    .

---
+++ Aula 1 - Conversor de Moedas/a.js
@@ -0,0 +1,9 @@
+function etherToReal(){
+    var ethereum = document.getElementById('valor').value;
+    var ethereumsEmReais = 8081.59;
+    var moedaEmReal = ethereumsEmReais * ethereum;
+    alert(ethereum + " ethers é igual a R$ " + moedaEmReal);
+    }  
+
+// IMERSÃO DEV
+// AUTOR: SAMUEL CAMARGO 
