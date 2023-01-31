49c8cd2cc540e0e38e1397ddc4768e055a88e3fd
SamukaCode <samuelpcamargo2005@gmail.com>
2023-01-31 15:52:26 -0300

    aula 2 finalizada

---
+++ Aula 2 - Mentalista/game.js
@@ -0,0 +1,17 @@
+var numeroSecreto = parseInt(Math.random() * 1001)
+
+var tentativas = 10
+
+while(tentativas > 0) {
+var chute = prompt('Digite um número entre 0 e 1000')
+tentativas = tentativas - 1
+if (chute == numeroSecreto) {
+    alert('Acertou!')    
+}
+else if (chute > numeroSecreto){
+    alert('Errou... o número secreto é menor que ' + chute + '\n Restam ' + tentativas + ' tentativas')
+}
+else if (chute < numeroSecreto){
+    alert('Errou... o número secreto é maior que ' + chute + '\n Restam ' + tentativas + ' tentativas')
+}
+}
